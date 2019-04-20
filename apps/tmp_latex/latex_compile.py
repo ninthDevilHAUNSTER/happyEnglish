c687f350c8515c72cdef5ef6b1f2a92b6ebdfd60
ninthDevilHAUNSTER <luotianhan@126.com>
2019-04-20 15:33:58 +0800

    更新latex编译函数

--- apps/tmp_latex/latex_compile.py
+++ apps/tmp_latex/latex_compile.py
@@ -1,35 +1,70 @@
-from tmp_latex.latex_word_list_config import *
-from tmp_latex.latex_answer_list_config import SENTENCE_ANSWER_MIDDLE_WARE, WORD_ANSWER_MIDDLE_WARE, \
-    WORD_ANSWER_MIDDLE_WARE_SINGLE_CASE, PDF_LATEX_COMPILE_CMD_ANSWER, HEADER_ANSWER
-from tmp_latex.latex_config import latex_header_gen
 import os
 from datetime import datetime
 import shutil
 from happyEnglish import settings
+import tmp_latex.latex_config as la
+from tmp_latex.latex_config import TMP_LATEX_PATH
 
 
-def compile_answers(words, sentences, user="匿名"):
-    latex = latex_header_gen(title="每日单词答案", user_name=user)
+def dictation_paper_compiler(words, sentences, user="匿名"):
+    latex = la.latex_header_gen(title="每日单词", user_name=user)
     if words.__len__() > 0:
-        latex += WORD_START
+        latex += la.word_part_start()
 
-    for word_index in range(0, words.__len__(), 2):
-        try:
-            latex += WORD_ANSWER_MIDDLE_WARE % (str(word_index + 1), words[word_index][0], words[word_index][1],
-                                                str(word_index + 2), words[word_index + 1][0], words[word_index + 1][1])
-        except IndexError:
-            latex += WORD_ANSWER_MIDDLE_WARE_SINGLE_CASE % (str(word_index), words[word_index][0], words[word_index][1])
-
+        for word_index in range(0, words.__len__(), 2):
+            try:
+                latex += la.word_part_double_column_with_underline(
+                    index1=str(word_index + 1), word1=words[word_index][0],
+                    index2=str(word_index + 2), word2=words[word_index + 1][0]
+                )
+            except IndexError:
+                latex += la.word_part_single_column_with_underline(
+                    index=str(word_index + 1), word=words[word_index][0]
+                )
     if sentences.__len__() > 0:
-        latex += SENTENCE_START
+        latex += la.sentence_part_start()
 
-    for sentence_index in range(0, sentences.__len__()):
-        latex += SENTENCE_ANSWER_MIDDLE_WARE % (
-            str(sentence_index + 1), sentences[sentence_index][0], sentences[sentence_index][1])
-    latex += TAIL
+        for sentence_index in range(0, sentences.__len__()):
+            latex += la.sentence_part_with_underline(
+                index=str(sentence_index + 1), sentence=sentences[sentence_index][0]
+            )
+    latex += la.tail()
+
+    open(TMP_LATEX_PATH + "\\tmp.tex", 'w', encoding="utf8").write(latex)
+    os.system(la.compile_cmd(TMP_LATEX_PATH + '\\tmp.tex'))
+    os.remove('tmp.aux')
+    os.remove('tmp.log')
+    save_path = settings.MEDIA_ROOT + "\\pdfs\\"
+    f_name = datetime.strftime(datetime.now(), 'words_%y_%m_%d_%H_%M') + ".pdf"
+    shutil.move('tmp.pdf', "{}".format(save_path + f_name))
+    return f_name
+
+
+def answer_sheet_compiler(words, sentences, user="匿名"):
+    latex = la.latex_header_gen("每日单词答案", user_name=user)
+    if words.__len__() > 0:
+        latex += la.word_part_start()
+        for word_index in range(0, words.__len__(), 2):
+            try:
+                latex += la.word_part_double_column_with_answer(
+                    index1=str(word_index + 1), word1=words[word_index][0], answer1=words[word_index][1],
+                    index2=str(word_index + 2), word2=words[word_index + 1][0], answer2=words[word_index + 1][1],
+                )
+            except IndexError:
+                latex += la.word_part_single_column_with_answer(
+                    index=str(word_index + 1), word=words[word_index][0], answer=words[word_index][1]
+                )
+    if sentences.__len__() > 0:
+        latex += la.sentence_part_start()
+        for sentence_index in range(0, sentences.__len__()):
+            latex += la.sentence_part_with_answer(
+                index=sentence_index, sentence=sentences[sentence_index][0], answer=sentences[sentence_index][1]
+            )
+
+    latex += la.tail()
 
     open(TMP_LATEX_PATH + "\\ans.tex", 'w', encoding="utf8").write(latex)
-    os.system(PDF_LATEX_COMPILE_CMD_ANSWER)
+    os.system(la.compile_cmd(TMP_LATEX_PATH + '\\ans.tex'))
     os.remove('ans.aux')
     os.remove('ans.log')
     save_path = settings.MEDIA_ROOT + "\\pdfs\\"
@@ -38,30 +73,14 @@ def compile_answers(words, sentences, user="匿名"):
     return f_name
 
 
-def compile_words(words, sentences, user="匿名"):
-    latex = latex_header_gen(title="每日单词", user_name=user)
-    if words.__len__() > 0:
-        latex += WORD_START
+def test_compile(words, sentences):
+    words = [('幻觉', 'delusion'), ('grafication', '满意的')]
+    sentences = [
+        ('In the word , two things are infinite , human\'s fool and universe, and I\'m sure what universe is .',
+         '在这个世界，有两样东西是无限的，宇宙和人类的愚蠢，但我知道宇宙是什么')]
+    dictation_paper_compiler(words, sentences)
+    answer_sheet_compiler(words, sentences)
 
-    for word_index in range(0, words.__len__(), 2):
-        try:
-            latex += WORD_MIDDLE_WARE % (str(word_index + 1), words[word_index][0],
-                                         str(word_index + 2), words[word_index + 1][0])
-        except IndexError:
-            latex += WORD_MIDDLE_WARE_SINGLE_CASE % (str(word_index), words[word_index][0])
 
-    if sentences.__len__() > 0:
-        latex += SENTENCE_START
-
-    for sentence_index in range(0, sentences.__len__()):
-        latex += SENTENCE_MIDDLE_WARE % (str(sentence_index + 1), sentences[sentence_index][0])
-    latex += TAIL
-
-    open(TMP_LATEX_PATH + "\\tmp.tex", 'w', encoding="utf8").write(latex)
-    os.system(PDF_LATEX_COMPILE_CMD)
-    os.remove('tmp.aux')
-    os.remove('tmp.log')
-    save_path = settings.MEDIA_ROOT + "\\pdfs\\"
-    f_name = datetime.strftime(datetime.now(), 'words_%y_%m_%d_%H_%M') + ".pdf"
-    shutil.move('tmp.pdf', "{}".format(save_path + f_name))
-    return f_name
+if __name__ == '__main__':
+    test_compile(1, 1)
--- apps/tmp_latex/latex_config.py
+++ apps/tmp_latex/latex_config.py
@@ -1,12 +1,9 @@
 import os
-from collections import OrderedDict
 
 TMP_LATEX_PATH = r"D:\python_box\happyEnglish\apps\tmp_latex"
-COMPILE_CMD = lambda filename: "pdflatex {} >nul".format(TMP_LATEX_PATH + filename)
 
-LATEX_WRITER_DICT = OrderedDict(
-    {
-        "HEADER": r"""\documentclass[a4paper, 12pt]{article}
+LATEX_WRITER_DICT = {
+    "HEADER": r"""\documentclass[a4paper, 12pt]{article}
 \usepackage[UTF8]{ctex}
 \usepackage{multicol}
 \usepackage{geometry}
@@ -19,14 +16,70 @@ LATEX_WRITER_DICT = OrderedDict(
     \author{ %s }
     \maketitle
 """,
-        "WORD_START": """
+    "WORD_PART_START": """
 \\begin{flushleft}
 一. 单词部分
 \\end{flushleft}
-"""
+""",
+    "WORD_PART_DOUBLE_COLUMN_WITH_UNDERLINE": """
+\\begin{multicols}{2}
+\\begin{flushleft}
+%s.\ %s \ \ \ \ \\underline{\hspace{3cm}}
+\\end{flushleft}
 
-    }
-)
+\\begin{flushleft}
+%s.\ %s \ \ \ \ \\underline{\hspace{3cm}}
+\\end{flushleft}
+\\end{multicols}
+""",
+    "WORD_PART_SINGLE_COLUMN_WITH_UNDERLINE": """
+\\begin{multicols}{2}
+\\begin{flushleft}
+%s.\ %s \ \ \ \ \\underline{\hspace{3cm}}
+\\end{flushleft}
+\\end{multicols}
+""",
+    "WORD_PART_DOUBLE_COLUMN_WITH_ANSWER": """
+\\begin{multicols}{2}
+\\begin{flushleft}
+%s.\ %s \ %s
+\\end{flushleft}
+
+\\begin{flushleft}
+%s.\ %s \ %s
+\\end{flushleft}
+\\end{multicols}
+""",
+    "WORD_PART_SINGLE_COLUMN_WITH_ANSWER": """
+\\begin{multicols}{2}
+\\begin{flushleft}
+%s.\ %s \ \ \ \ \\underline{\hspace{3cm}}
+\\end{flushleft}
+\\end{multicols}
+""",
+    "SENTENCE_PART_START": """
+\\begin{flushleft}
+二. 句子部分
+\\end{flushleft}
+""",
+    "SENTENCE_PART_WITH_UNDERLINE": """
+\\begin{flushleft}
+%s.\ %s
+
+\\underline{\\hspace{16cm}}
+\\end{flushleft}
+""",
+    "SENTENCE_PART_WITH_ANSWER": """
+\\begin{flushleft}
+%s.\ %s
+
+%s
+\\end{flushleft}
+""",
+    "TAIL": r"""
+\end{document}
+""",
+}
 
 
 def latex_header_gen(title="每日单词", user_name="烧包"):
@@ -35,3 +88,66 @@ def latex_header_gen(title="每日单词", user_name="烧包"):
         title,
         user_name
     )
+
+
+def word_part_start():
+    return LATEX_WRITER_DICT["WORD_PART_START"]
+
+
+def word_part_double_column_with_underline(index1, word1, index2, word2):
+    return LATEX_WRITER_DICT['WORD_PART_DOUBLE_COLUMN_WITH_UNDERLINE'] % (index1, word1, index2, word2)
+
+
+def word_part_single_column_with_underline(index, word):
+    return LATEX_WRITER_DICT['WORD_PART_SINGLE_COLUMN_WITH_UNDERLINE'] % (index, word)
+
+
+def word_part_double_column_with_answer(index1, word1, answer1, index2, word2, answer2):
+    return LATEX_WRITER_DICT['WORD_PART_DOUBLE_COLUMN_WITH_ANSWER'] % (
+        index1, word1, answer1, index2, word2, answer2
+    )
+
+
+def word_part_single_column_with_answer(index, word, answer):
+    return LATEX_WRITER_DICT['WORD_PART_SINGLE_COLUMN_WITH_ANSWER'] % (
+        index, word, answer
+    )
+
+
+def sentence_part_start():
+    return LATEX_WRITER_DICT['SENTENCE_PART_START']
+
+
+def sentence_part_with_underline(index, sentence):
+    return LATEX_WRITER_DICT['SENTENCE_PART_WITH_UNDERLINE'] % (
+        index, sentence
+    )
+
+
+def sentence_part_with_answer(index, sentence, answer):
+    return LATEX_WRITER_DICT['SENTENCE_PART_WITH_ANSWER'] % (
+        index, sentence, answer
+    )
+
+
+def tail():
+    return LATEX_WRITER_DICT['TAIL']
+
+
+def compile_cmd(file_name):
+    if "linux" in os.sys.platform:
+        return "".join(i + " " for i in [
+            "xelatex",
+            # "-interaction=batchmode",
+            # "-halt-on-error",
+            file_name,
+            "1>/dev/null 2>/dev/null &"])
+    elif "win" in os.sys.platform:
+        return "".join(i + " " for i in [
+            "xelatex",
+            # "-interaction=batchmode",
+            # "-halt-on-error",
+            file_name,
+            ">nul"])
+    else:
+        return False
